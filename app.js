4b53416fb187c207b1e3657ae87f3b19ef2c9f37
Ali Ahmed Khan <aliahmed.k30062@gmail.com>
2025-01-01 18:27:59 +0500

    added alot

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 require("dotenv").config();
 var express = require("express");
 var cors = require("cors");
-var mongoose = require("mongoose");
-var Cookies = require("cookies");
-var authenticationRouter = require("./routes/auth.routes");
-var userRouter = require("./routes/user.routes")
-var indexRouter = require("./routes/index.routes")
 var path = require("path");
-const multer  = require('multer')
+var mongoose = require("mongoose");
+const superuser = require("./models/superuser");
 
 
-//Get port from environment and store in Express.
-const port = process.env.PORT || "3000";
-
-
-//DATABASE CONNECTION 
-
+//DATABASE CONNECTION
 
 var db = mongoose
-  .connect(process.env.conString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.conString)
   .then(() => {
     console.log("Successfully connect to MongoDB.");
   })
@@ -30,49 +18,30 @@ var db = mongoose
     process.exit();
   });
 
+//Express APP
 var app = express();
 
-var corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
-
 //MIDDLEWARES
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors(corsOptions));
-app.use(Cookies.express());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 //ROUTES
+superuserRouter = require("./routes/superuser.route")
+app.use("/superuser", superuserRouter);
 
-
-app.use("/auth", authenticationRouter);
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './public/images/')
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, uniqueSuffix + '-' + file.originalname)
-  }
-})
-
-const upload = multer({ storage: storage})
-
-const verifyToken = require("./middlewares/verifyJwt")
-
-app.use("/user", verifyToken, upload.array("car_images", 6), userRouter)
-
-app.use("/", indexRouter);
 
 
 //DEVELOPMENT SERVER
 
-
+//Get port from environment and store in Express.
+const port = process.env.PORT || "3000";
 app.listen(port);
 console.log("Server listening on port " + port);
 console.log(`Visit http://localhost:${port}/`);
