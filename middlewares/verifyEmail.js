0f3cbb6bd47641b4cc9f1ea6d5007d712f172751
Ali Ahmed Khan <aliahmed.k30062@gmail.com>
2025-08-14 12:30:38 +0500

    fixed student registration problem

--- middlewares/verifyEmail.js
+++ middlewares/verifyEmail.js
@@ -5,25 +5,29 @@ verifyEmail = async (req, res, next) => {
 
     const url = `https://api.quickemailverification.com/v1/verify?email=${req.body.email}&apikey=${process.env.API_KEY}`
     try {
-      console.log('0')
+      
       const response = await fetch(url)
-      console.log('1')
+      
       if (response.status === 200) {
         const resJson = await response.json()
-        console.log('2')
+        
+        //if invalid send invalid status
         if(resJson.result === "invalid")
         {
             res.status(400).send({isEmailValid: false, message: "Email address does not exist, Pleae enter a valid Email", code: 'INVALID_EMAIL'})
             return;
+        } else {
+          //if valid and user is a system admin, that means he is creating an admin account, so call the next
+          //middleware in the chain
+          if(req.user) {
+            if(req.user.role === 'system_admin') next();
+          } 
+          //if valid and user is not set, that means student is creating an account
+          else return res.status(200).send({isEmailValid: true, message: "Email address exists", code: 'VALID_EMAIL'});
         }
-        if(req.user) {
-          if(req.user.role === 'system_admin') next();
-        }
+        
 
         }
-
-        else return res.status(200).send({isEmailValid: true, message: "Email address exists", code: 'VALID_EMAIL'});
-
       }
 
       catch (err) {
